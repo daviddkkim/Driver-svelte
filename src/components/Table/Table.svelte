9f02ed64656699c939a2d00e78e24c193a766f9c
daviddkkim <david.kim@richmond.edu>
2023-05-02 22:14:01 -0700

    make table rows work

--- src/components/Table/Table.svelte
+++ src/components/Table/Table.svelte
@@ -1,32 +1,44 @@
 <script lang="ts">
-	const TableContainer = `bg-white border border-t-0 border-l-0 rounded-sm text-stone-700 w-full h-fit`;
-	const TableHeader = `flex border-b`;
-	const HeaderCell = `bg-stone-50 font-medium px-4 py-1  border-r text-stone-700 hover:bg-stone-100 w-full last:border-r-0 last:rounded-r-sm first:rounded-l-sm`;
-	const TableRow = ` flex border-b last:border-b-0`;
-	const TableCell = `bg-white px-4 py-1  border-r text-stone-700 hover:bg-stone-100 w-full last:border-r-0 last:rounded-r-sm first:rounded-l-sm`;
+	const TableContainer = `bg-white border border-b-0 border-t-0 border-l-0 rounded-sm text-stone-700 w-full h-fit`;
+	const TableHeader = `flex border-b bg-stone-50`;
+	const HeaderCell = `bg-stone-50 font-medium text-sm px-4 py-1  border-r text-stone-700 hover:bg-stone-100 min-w-[150px] last:rounded-r-sm first:rounded-l-sm`;
+	const TableRow = ` flex`;
+	const TableCell = `bg-white px-4 py-1 text-sm  border-r border-b text-stone-700 hover:bg-stone-100 min-w-[150px] last:rounded-r-smfirst:rounded-l-sm`;
+
+	export let data = [
+		{ firstname: 'John', lastname: 'Doe' },
+		{ firstname: 'Jane', lastname: 'Doe' }
+	];
+	export let column = [
+		{
+			header: 'firstname'
+		},
+		{
+			header: 'lastname'
+		}
+	];
+
+	const sortedData = data.map((row) => {
+		return Object.values(row);
+	});
 </script>
 
 <div class={TableContainer}>
 	<div>
 		<div class={TableHeader}>
-			<div class={HeaderCell}>header</div>
-			<div class={HeaderCell}>header</div>
-			<div class={HeaderCell}>header</div>
-			<div class={HeaderCell}>header</div>
+			{#each column as col}
+				<div class={HeaderCell}>{col.header}</div>
+			{/each}
+			<button> +</button>
 		</div>
 	</div>
 	<div>
-		<div class={TableRow}>
-			<div class={TableCell}>body</div>
-			<div class={TableCell}>body</div>
-			<div class={TableCell}>body</div>
-			<div class={TableCell}>body</div>
-		</div>
-		<div class={TableRow}>
-			<div class={TableCell}>body</div>
-			<div class={TableCell}>body</div>
-			<div class={TableCell}>body</div>
-			<div class={TableCell}>body</div>
-		</div>
+		{#each sortedData as row}
+			<div class={TableRow}>
+				{#each row as cell}
+					<div class={TableCell}>{cell}</div>
+				{/each}
+			</div>
+		{/each}
 	</div>
 </div>
