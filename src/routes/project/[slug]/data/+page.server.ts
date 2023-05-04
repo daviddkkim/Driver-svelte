bbcaf43760ed4d8e1a1095bdf751c9364f1bfbca
daviddkkim <david.kim@richmond.edu>
2023-05-03 23:17:18 -0700

    get column adding working

--- src/routes/project/[slug]/data/+page.server.ts
+++ src/routes/project/[slug]/data/+page.server.ts
@@ -7,8 +7,7 @@ export async function load() {
     const { rows: columns } = await client.query(`SELECT column_name, data_type, character_maximum_length
     FROM information_schema.columns
     WHERE table_name = 'driver';`)
-    console.log(dataRows)
-    console.log(columns)
+
     return {
         data: dataRows,
         columns: columns
@@ -17,11 +16,19 @@ export async function load() {
 
 export const actions = {
     addColumn: async ({ request }) => {
+        console.log('here')
         const data = await request.formData();
-      /*   console.log(data)
-        const { rows: dataRows } = await client.query(`ALTER TABLE driver ADD COLUMN ${data.get('column_name')} ${data.get('data_type')}(${data.get('character_maximum_length')})`); */
-        return {
-            data
+        console.log(data.get('column_name'))
+        console.log(data.get('column_type'))
+        try {
+            const { rows: dataRows } = await client.query(`ALTER TABLE driver ADD COLUMN ${data.get('column_name')} ${data.get('column_type')}`);
+            console.log(dataRows)
+            return {
+                dataRows
+            }
+        }
+        catch (e) {
+            console.log(e)
         }
         // TODO log the user in
     }
--- src/routes/project/[slug]/data/+page.svelte
+++ src/routes/project/[slug]/data/+page.svelte
@@ -29,23 +29,37 @@
 			<div
 				class={'z-20 absolute bg-white border inset-2/4 -translate-y-2/4 -translate-x-2/4 max-w-[400px] w-full h-fit rounded-sm shadow-lg'}
 			>
-				<form>
+				<form method="POST" action="?/addColumn">
 					<div class={`p-3`}>
-						<label class={'flex flex-col text-sm mb-3'}>
+						<label class={'flex flex-col text-sm mb-3'} for="columnName">
 							Column name
-							<input class={'border rounded-sm p-2 text-sm'} type="text" />
+							<input
+								class={'border rounded-sm p-2 text-sm'}
+								type="text"
+								id="columnName"
+								name="column_name"
+							/>
 						</label>
-						<label class={'flex flex-col text-sm'}>
+						<label class={'flex flex-col text-sm'} for="columnType">
 							Column type
-							<select class={`border rounded-sm p-2 appearance-none text-sm`} value={colType}>
+							<select
+								class={`border rounded-sm p-2 appearance-none text-sm`}
+								value={colType}
+								id="columnType"
+								name="column_type"
+							>
 								<option value="text">text</option>
 								<option value="checkbox">checkbox</option>
 							</select>
 						</label>
 					</div>
 					<div class={'w-full p-3 border-t bg-stone-50 flex justify-end'}>
-						<label class={'text-sm flex items-center mr-auto'}> <input type="checkbox" class={'mr-1'} /> Create another column</label>
-						<button class={'bg-orange-600 text-white text-sm px-2 py-1 rounded-sm'}> Save</button>
+						<label class={'text-sm flex items-center mr-auto'}>
+							<input type="checkbox" class={'mr-1'} /> Create another column</label
+						>
+						<button class={'bg-orange-600 text-white text-sm px-2 py-1 rounded-sm'} type="submit">
+							Save</button
+						>
 					</div>
 				</form>
 			</div>
