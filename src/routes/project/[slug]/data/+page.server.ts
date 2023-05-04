05050e866e4ddd51e4773ca43946a48a43b3e27e
daviddkkim <david.kim@richmond.edu>
2023-05-03 21:34:05 -0700

    get columns and data pull working

--- src/routes/project/[slug]/data/+page.server.ts
+++ src/routes/project/[slug]/data/+page.server.ts
@@ -3,10 +3,14 @@ import { client } from "../../../../$lib/server/db/postgres";
 export async function load() {
 
 
-    const res = await client.query('SELECT NOW()');
-
-    console.log(res)
+    const { rows: dataRows } = await client.query('SELECT * from driver');
+    const { rows: columns } = await client.query(`SELECT column_name, data_type, character_maximum_length
+    FROM information_schema.columns
+    WHERE table_name = 'driver';`)
+    console.log(dataRows)
+    console.log(columns)
     return {
-        data: res.rows
+        data: dataRows,
+        columns: columns
     };
 }
--- src/routes/project/[slug]/data/+page.svelte
+++ src/routes/project/[slug]/data/+page.svelte
@@ -1,10 +1,18 @@
 <script lang="ts">
-	import { Table, SearchIcon } from '../../../../components';
+	import { Table } from '../../../../components';
 
 	export let data;
+
+	console.log(data)
+	const columns = data.columns.map((col) => {
+		return {
+			header: col.column_name
+		}
+	});
+
 </script>
 
 <div class={'flex flex-col w-full'}>
 	<div class={'flex items-center px-4 py-2 w-full border-b border-stone-200'}>Tool bar</div>
-	<Table data={data.data} />
+	<Table data={data.data} column={columns} />
 </div>
